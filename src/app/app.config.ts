5ae38fabc1820e6e6b080e5107a2111bfcfb1d56
J Rob Gant <rgant@alum.wpi.edu>
2025-01-09 18:55:01 -0500

    Install cypress

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,29 +1,29 @@
 /* eslint-disable import-x/max-dependencies -- config files tend to have a lot of dependencies */
 import { provideZoneChangeDetection } from '@angular/core';
 import type { ApplicationConfig } from '@angular/core';
-import {
-  getAnalytics,
-  provideAnalytics,
-  ScreenTrackingService,
-  UserTrackingService,
-} from '@angular/fire/analytics';
-import type { Analytics } from '@angular/fire/analytics';
+// import {
+//   getAnalytics,
+//   provideAnalytics,
+//   ScreenTrackingService,
+//   UserTrackingService,
+// } from '@angular/fire/analytics';
+// import type { Analytics } from '@angular/fire/analytics';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import type { FirebaseApp } from '@angular/fire/app';
-import { initializeAppCheck, provideAppCheck, ReCaptchaEnterpriseProvider } from '@angular/fire/app-check';
-import type { AppCheck } from '@angular/fire/app-check';
+// import { initializeAppCheck, provideAppCheck, ReCaptchaEnterpriseProvider } from '@angular/fire/app-check';
+// import type { AppCheck } from '@angular/fire/app-check';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import type { Auth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import type { Firestore } from '@angular/fire/firestore';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
-import type { Functions } from '@angular/fire/functions';
+// import { getFunctions, provideFunctions } from '@angular/fire/functions';
+// import type { Functions } from '@angular/fire/functions';
 import { getPerformance, providePerformance } from '@angular/fire/performance';
 import type { FirebasePerformance } from '@angular/fire/performance';
-import { getStorage, provideStorage } from '@angular/fire/storage';
-import type { FirebaseStorage } from '@angular/fire/storage';
-import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
-import type { VertexAI } from '@angular/fire/vertexai';
+// import { getStorage, provideStorage } from '@angular/fire/storage';
+// import type { FirebaseStorage } from '@angular/fire/storage';
+// import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
+// import type { VertexAI } from '@angular/fire/vertexai';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -44,17 +44,17 @@ export const appConfig: ApplicationConfig = {
         }),
     ),
     provideAuth((): Auth => getAuth()),
-    provideAnalytics((): Analytics => getAnalytics()),
-    ScreenTrackingService,
-    UserTrackingService,
-    provideAppCheck((): AppCheck => {
-      const provider = new ReCaptchaEnterpriseProvider('6LfG3rEqAAAAAK18S9UyMuK8wd3JSWjVnmDzmlrT');
-      return initializeAppCheck(undefined, { isTokenAutoRefreshEnabled: true, provider });
-    }),
+    // provideAnalytics((): Analytics => getAnalytics()),
+    // ScreenTrackingService,
+    // UserTrackingService,
+    // provideAppCheck((): AppCheck => {
+    //   const provider = new ReCaptchaEnterpriseProvider('6LfG3rEqAAAAAK18S9UyMuK8wd3JSWjVnmDzmlrT');
+    //   return initializeAppCheck(undefined, { isTokenAutoRefreshEnabled: true, provider });
+    // }),
     provideFirestore((): Firestore => getFirestore()),
-    provideFunctions((): Functions => getFunctions()),
+    // provideFunctions((): Functions => getFunctions()),
     providePerformance((): FirebasePerformance => getPerformance()),
-    provideStorage((): FirebaseStorage => getStorage()),
-    provideVertexAI((): VertexAI => getVertexAI()),
+    // provideStorage((): FirebaseStorage => getStorage()),
+    // provideVertexAI((): VertexAI => getVertexAI()),
   ],
 };
