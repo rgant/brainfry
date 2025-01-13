b3e3689a876aec552ef7b8249d2d09752920d18e
J Rob Gant <rgant@alum.wpi.edu>
2025-01-13 07:59:54 -0500

    fix default AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import type { ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', (): void => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async (): Promise<void> => {
     await TestBed.configureTestingModule({
       imports: [ AppComponent ],
-    }).compileComponents();
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', (): void => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it("should have the 'brainfry' title", (): void => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('brainfry');
-  });
-
-  it('should render title', (): void => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as unknown;
-    if (!(compiled instanceof HTMLElement)) {
-      throw new TypeError('nativeElement is not HTMLElement');
-    }
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, brainfry');
+    expect(component).toBeTruthy();
   });
 });
