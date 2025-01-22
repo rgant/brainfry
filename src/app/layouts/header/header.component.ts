904ec9170b4ebb258d4e42123f80d15f9de81fe1
J Rob Gant <rgant@alum.wpi.edu>
2025-01-22 13:55:12 -0500

    style standard layout and header for mobile first

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,13 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [ NgOptimizedImage ],
+  imports: [ NgOptimizedImage, RouterLink ],
   selector: 'app-header',
   styleUrl: './header.component.scss',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
