16d1aeaf18be479729f28fb1ae902410ceb63dc5
J Rob Gant <rgant@alum.wpi.edu>
2025-02-11 12:33:31 -0500

    lazy load infrequent routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,28 @@
 /* eslint-disable @angular-eslint/runtime-localize -- `Route.title`s may not be translated using `loadTranslations()` */
 /* eslint-disable import-x/max-dependencies -- routes files tend to have a lot of dependencies */
+import type { Type as ComponentType } from '@angular/core';
 import type { Routes } from '@angular/router';
 
 import { authGuard, emailVerifiedGuard, noAuthGuard } from './core/guards';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ActionsComponent } from './identity/actions/actions.component';
-import { ChangeEmailComponent } from './identity/change-email/change-email.component';
-import { ChangePasswordComponent } from './identity/change-password/change-password.component';
-import { ConfirmEmailComponent } from './identity/confirm-email/confirm-email.component';
-import { DeleteAccountComponent } from './identity/delete-account/delete-account.component';
-import { ForgotPasswordComponent } from './identity/forgot-password/forgot-password.component';
+import type { ActionsComponent } from './identity/actions/actions.component';
+import type { ChangeEmailComponent } from './identity/change-email/change-email.component';
+import type { ChangePasswordComponent } from './identity/change-password/change-password.component';
+import type { ConfirmEmailComponent } from './identity/confirm-email/confirm-email.component';
+import type { DeleteAccountComponent } from './identity/delete-account/delete-account.component';
+import type { ForgotPasswordComponent } from './identity/forgot-password/forgot-password.component';
 import { LoginComponent } from './identity/login/login.component';
-import { LogoutComponent } from './identity/logout/logout.component';
-import { RecoverEmailComponent } from './identity/recover-email/recover-email.component';
-import { ResetPasswordComponent } from './identity/reset-password/reset-password.component';
-import { PhotoManagerComponent } from './identity/user-photos/photo-manager/photo-manager.component';
-import { UserProfileComponent } from './identity/user-profile/user-profile.component';
-import { VerifyEmailComponent } from './identity/verify-email/verify-email.component';
+import type { LogoutComponent } from './identity/logout/logout.component';
+import type { RecoverEmailComponent } from './identity/recover-email/recover-email.component';
+import type { ResetPasswordComponent } from './identity/reset-password/reset-password.component';
+import type { PhotoManagerComponent } from './identity/user-photos/photo-manager/photo-manager.component';
+import type { UserProfileComponent } from './identity/user-profile/user-profile.component';
+import type { VerifyEmailComponent } from './identity/verify-email/verify-email.component';
 import { CentralLayoutComponent } from './layouts/central/central.component';
 import { StandardLayoutComponent } from './layouts/standard/standard.component';
-import { PrivacyPolicyComponent } from './legal/privacy-policy/privacy-policy.component';
-import { TermsAndConditionsComponent } from './legal/terms-and-conditions/terms-and-conditions.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import type { PrivacyPolicyComponent } from './legal/privacy-policy/privacy-policy.component';
+import type { TermsAndConditionsComponent } from './legal/terms-and-conditions/terms-and-conditions.component';
+import type { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   // Needs to be a destination inside authenticated area.
@@ -36,7 +37,10 @@ export const routes: Routes = [
     children: [
       {
         path: 'forgot-password',
-        component: ForgotPasswordComponent,
+        loadComponent: async (): Promise<ComponentType<ForgotPasswordComponent>> => {
+          const mod = await import('./identity/forgot-password/forgot-password.component');
+          return mod.ForgotPasswordComponent;
+        },
         title: $localize`:HTML title tag|Send password recover email@@htmlTitle.forgot-password:Forgot Password`,
       },
       {
@@ -46,7 +50,10 @@ export const routes: Routes = [
       },
       {
         path: 'reset-password',
-        component: ResetPasswordComponent,
+        loadComponent: async (): Promise<ComponentType<ResetPasswordComponent>> => {
+          const mod = await import('./identity/reset-password/reset-password.component');
+          return mod.ResetPasswordComponent;
+        },
         title: $localize`:HTML title tag|Reset forgotten password@@htmlTitle.reset-password:Reset Password`,
       },
     ],
@@ -61,12 +68,18 @@ export const routes: Routes = [
     children: [
       {
         path: 'change-email',
-        component: ChangeEmailComponent,
+        loadComponent: async (): Promise<ComponentType<ChangeEmailComponent>> => {
+          const mod = await import('./identity/change-email/change-email.component');
+          return mod.ChangeEmailComponent;
+        },
         title: $localize`:HTML title tag|Change account email address@@htmlTitle.change-email:Change your email address`,
       },
       {
         path: 'change-password',
-        component: ChangePasswordComponent,
+        loadComponent: async (): Promise<ComponentType<ChangePasswordComponent>> => {
+          const mod = await import('./identity/change-password/change-password.component');
+          return mod.ChangePasswordComponent;
+        },
         title: $localize`:HTML title tag|Change account password@@htmlTitle.change-password:Change your password`,
       },
       {
@@ -76,23 +89,35 @@ export const routes: Routes = [
       },
       {
         path: 'delete-account',
-        component: DeleteAccountComponent,
+        loadComponent: async (): Promise<ComponentType<DeleteAccountComponent>> => {
+          const mod = await import('./identity/delete-account/delete-account.component');
+          return mod.DeleteAccountComponent;
+        },
         title: $localize`:HTML title tag|Delete account@@htmlTitle.delete-account:Delete your account`,
       },
       {
         // Only Users with verified emails can logout. Is that the correct choice?
         path: 'logout',
-        component: LogoutComponent,
+        loadComponent: async (): Promise<ComponentType<LogoutComponent>> => {
+          const mod = await import('./identity/logout/logout.component');
+          return mod.LogoutComponent;
+        },
         title: $localize`:HTML title tag|Logout of session@@htmlTitle.logout:Logout`,
       },
       {
         path: 'photos',
-        component: PhotoManagerComponent,
+        loadComponent: async (): Promise<ComponentType<PhotoManagerComponent>> => {
+          const mod = await import('./identity/user-photos/photo-manager/photo-manager.component');
+          return mod.PhotoManagerComponent;
+        },
         title: $localize`:HTML title tag|Page displaying profile photo uploads@@htmlTitle.photo:Your Photos`,
       },
       {
         path: 'profile',
-        component: UserProfileComponent,
+        loadComponent: async (): Promise<ComponentType<UserProfileComponent>> => {
+          const mod = await import('./identity/user-profile/user-profile.component');
+          return mod.UserProfileComponent;
+        },
         title: $localize`:HTML title tag|Page displaying your user profile@@htmlTitle.profile:Your Profile`,
       },
     ],
@@ -107,41 +132,62 @@ export const routes: Routes = [
       {
         // Redirects the user from the Firebase email to our custom action URLs
         path: 'actions',
-        component: ActionsComponent,
+        loadComponent: async (): Promise<ComponentType<ActionsComponent>> => {
+          const mod = await import('./identity/actions/actions.component');
+          return mod.ActionsComponent;
+        },
         // title: '', // Redirect page with no need for title
       },
       {
         // Sends the user an email to confirm access to the email address
         path: 'confirm-email',
         canActivate: [ authGuard ],
-        component: ConfirmEmailComponent,
+        loadComponent: async (): Promise<ComponentType<ConfirmEmailComponent>> => {
+          const mod = await import('./identity/confirm-email/confirm-email.component');
+          return mod.ConfirmEmailComponent;
+        },
         title: $localize`:HTML title tag|Send email confirming address access@@htmlTitle.confirm-email:Confirm Email`,
       },
       {
         path: 'privacy',
-        component: PrivacyPolicyComponent,
+        loadComponent: async (): Promise<ComponentType<PrivacyPolicyComponent>> => {
+          const mod = await import('./legal/privacy-policy/privacy-policy.component');
+          return mod.PrivacyPolicyComponent;
+        },
         title: $localize`:HTML title tag|Legal document page@@htmlTitle.privacy:Privacy Policy`,
       },
       {
         // Reverts a change of email address on the account
         path: 'recover-email',
-        component: RecoverEmailComponent,
+        loadComponent: async (): Promise<ComponentType<RecoverEmailComponent>> => {
+          const mod = await import('./identity/recover-email/recover-email.component');
+          return mod.RecoverEmailComponent;
+        },
         title: $localize`:HTML title tag|Reverts email address change@@htmlTitle.recover-email:Recover Email`,
       },
       {
         path: 'terms',
-        component: TermsAndConditionsComponent,
+        loadComponent: async (): Promise<ComponentType<TermsAndConditionsComponent>> => {
+          const mod = await import('./legal/terms-and-conditions/terms-and-conditions.component');
+          return mod.TermsAndConditionsComponent;
+        },
         title: $localize`:HTML title tag|Legal document page@@htmlTitle.terms:Terms & Conditions`,
       },
       {
         // Marks user account has having a verified email address
         path: 'verify-email',
-        component: VerifyEmailComponent,
+        loadComponent: async (): Promise<ComponentType<VerifyEmailComponent>> => {
+          const mod = await import('./identity/verify-email/verify-email.component');
+          return mod.VerifyEmailComponent;
+        },
         title: $localize`:HTML title tag|Marks email address is verified@@htmlTitle.verify-email:Verify Email`,
       },
       {
         path: '**',
-        component: PageNotFoundComponent,
+        loadComponent: async (): Promise<ComponentType<PageNotFoundComponent>> => {
+          const mod = await import('./page-not-found/page-not-found.component');
+          return mod.PageNotFoundComponent;
+        },
         title: $localize`:HTML title tag|Page not found 404 error@@htmlTitle.not-found:Page Not Found`,
       },
     ],
